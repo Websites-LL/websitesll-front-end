b50fb64587add49e124c9e181ebe841502109df9
Leo Fielstette <88216682+syph1000@users.noreply.github.com>
2022-01-16 18:24:55 +0100

    update

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
 
-const app = express()
-const port = process.env.PORT || 5000 
+const path = require('path');
 
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+if (err) return console.log(err);
+console.log('Server running on port: ', port);
+})
